6b2bbbac7a78edd161a928c61215dbaa7b730514
Eoin Roche <rochee6@tcd.ie>
2018-10-18 01:54:31 +0100

    added login page

--- blog-thing/app/Main.hs
+++ blog-thing/app/Main.hs
@@ -19,14 +19,27 @@ main = S.scotty 3000 $ do
         H.head $ 
           H.title "An Blog"  
         H.body $ do
+          topBar
           H.h1 "My Blog"
-                    
-          showLinks $ ["text/2", "text1/1"]
+          showLinks $ ["Pieces/Article1", "Pieces/Article2"]
 
-    S.get "/:title" $ do
-      title <- param "title"
-      file $ "Pieces/" ++ title  
+    S.get "Pieces/:title" $ do
+      --title <- param "title"
+      --file $ "Pieces/" ++ title
+      S.html . R.renderHtml $ do
+        H.head $
+          H.title "title"
+        H.body $ do
+          topBar
+    
 
+    S.get "/login" $ do
+      S.html . R.renderHtml $ do
+        H.head $ 
+          H.title "New Post"
+        H.body $ login
+         
+ 
 showLinks :: [H.AttributeValue] -> H.Markup  
 showLinks [] = error "empty list"
 showLinks (x:[]) = H.h2 $ H.a H.! href(x) $ "Post"
@@ -37,5 +50,25 @@ showLinks (x:xs) = do
 sortLinks :: [String] -> [String]
 sortLinks [] = error "empty list"
 
+topBar :: H.Html
+topBar = H.div H.! class_ "container" $ do
+           H.a H.! href("/") $ do
+             H.button H.! type_ "button" $ "Home" 
+           H.a H.! href("/newPost") $ do
+             H.button H.! type_ "button" $ "New Post"  
 
-       
+login :: H.Html
+login = do  
+             H.h1 "Login"
+             H.form H.! Text.Blaze.Html5.Attributes.id "pw" $ "Password (It's Wew)"
+             H.input H.! type_ "text"
+             H.button H.! onclick "checkPW()" $ "Click!"
+             H.h2 H.! Text.Blaze.Html5.Attributes.id "out" $ ""
+             H.script $ "function checkPW() { var x = documents.getElementById('pw'); var text = x.elements[0].value; document.getElementById('out').innerHTML = text; }" 
+
+
+
+
+
+
+
